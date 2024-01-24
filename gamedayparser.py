d841855a227088ba21e9835833d23d4d5d57ee2e
Ahmad Jamous <114758348+ajamous1@users.noreply.github.com>
2024-01-23 21:18:17 -0500

    Update gamedayparser.py
    Bug: entering the away team would duplicate the results. Resolved this issue by separating the home and away teams and executing accordingly

--- gamedayparser.py
+++ gamedayparser.py
@@ -11,14 +11,14 @@ class WebCrawler:
         self.first_link = link
         self.ID = num
         self.driver = self.setup_webdriver()
-        self.opponent = None
-        # Initialize opponent attribute
-        self.crawl(1, self.first_link)
+        self.home_team = None
+        self.away_team = None
+        # Initialize teams
+        self.crawl()
 
     def setup_webdriver(self):
         try:
             options = Options()
-            #Removes need to open webpage
             options.add_argument('--headless')
             driver_path = r'C:\Users\ahmad\Downloads\chromedriver-win64\chromedriver-win64\chromedriver.exe'
             return webdriver.Chrome(executable_path=driver_path, options=options)
@@ -26,27 +26,29 @@ class WebCrawler:
             print(f"Error setting up Chromedriver: {e}")
             exit(1)
 
-    def crawl(self, level, url):
-        if level <= 1:
-            self.driver.get(url)
-            time.sleep(2)
-            soup = BeautifulSoup(self.driver.page_source, 'html.parser')
-            team_names = soup.select_one('.MuiTypography-h5').text
-            raw_date = soup.select_one('.MuiTypography-caption.MatchSeason').text
-            location_elements = soup.select('h6.MuiTypography-h6')
-            location_data = location_elements[1] if location_elements else None
-            location = location_data.text.strip() if location_data else "N/A"
-            time_data = soup.select_one('.MuiTypography-caption.MatchTime').text
-            jerseys_info = soup.select('.customEditionChip .MuiChip-label')
-            jerseys = ', '.join([jersey.text for jersey in jerseys_info[:2]])
-            opponent_info = soup.select('h5.MuiTypography-h5')
-            self.opponent = opponent_info[1].text.strip() if opponent_info else "N/A"
-            formatted_date = self.format_date(raw_date)
-            self.process_and_output_data(url, team_names, jerseys, location, formatted_date, time_data, self.opponent)
+    def crawl(self):
+        self.driver.get(self.first_link)
+        time.sleep(2)
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        team_names = soup.select_one('.MuiTypography-h5').text
+        raw_date = soup.select_one('.MuiTypography-caption.MatchSeason').text
+        location_elements = soup.select('h6.MuiTypography-h6')
+        location_data = location_elements[1] if location_elements else None
+        location = location_data.text.strip() if location_data else "N/A"
+        time_data = soup.select_one('.MuiTypography-caption.MatchTime').text
+        jerseys_info = soup.select('.customEditionChip .MuiChip-label')
+        jerseys = ', '.join([jersey.text for jersey in jerseys_info[:2]])
+        opponent_info = soup.select('h5.MuiTypography-h5')
+        self.away_team = opponent_info[1].text.strip() if opponent_info else "N/A"
+        self.home_team = team_names.replace(self.away_team, "").strip()
+        formatted_date = self.format_date(raw_date)
 
-    def process_and_output_data(self, url, team_names, jerseys, location, date, time_data, opponent):
+        # Output data for both teams
+        self.process_and_output_data(self.home_team, self.away_team, jerseys, location, formatted_date, time_data)
+
+    def process_and_output_data(self, home_team, away_team, jerseys, location, date, time_data):
         try:
-            print(f"Processed data from {url}:\nHome Team: {team_names}\nAway Team: {opponent} \nJerseys: {jerseys}\nLocation: {location}\nDate: {date}\nTime: {time_data}")
+            print(f"Processed data:\nHome Team: {home_team}\nAway Team: {away_team} \nJerseys: {jerseys}\nLocation: {location}\nDate: {date}\nTime: {time_data}")
         except UnicodeEncodeError:
             print("UnicodeEncodeError: Unable to print some characters")
 
@@ -55,11 +57,6 @@ class WebCrawler:
 
     def quit_webdriver(self):
         self.driver.quit()
-
-def parse_record(record_text):
-    # Extracting the record
-    record = record_text.find('strong').next_sibling.strip().split(',')[0]
-    return record
 
 def get_last_5_games_results(team_name):
     base_url = f"https://www.statmuse.com/nba/ask/{team_name.lower().replace(' ', '-')}-last-5-games"
@@ -105,25 +102,25 @@ if __name__ == "__main__":
 
     team_crawler = WebCrawler(fr"https://lockervision.nba.com/team/{team_name.lower().replace(' ', '-')}", 1)
     
-    record_home, seeding_home = get_record_and_seeding(team_name)
+    record_home, seeding_home = get_record_and_seeding(team_crawler.home_team)
     if record_home and seeding_home:
         print(f"Home Record: {record_home}\nHome Seeding: {seeding_home}")
     else:
         print("Failed to fetch home record and seeding.")
 
-    last_5_games_results_home = get_last_5_games_results(team_name)
+    last_5_games_results_home = get_last_5_games_results(team_crawler.home_team)
     if last_5_games_results_home:
         print(f"Home Last 5 Games: {last_5_games_results_home}")
     else:
         print("Failed to fetch home last 5 games results.")
     
-    record_away, seeding_away = get_record_and_seeding(team_crawler.opponent)
+    record_away, seeding_away = get_record_and_seeding(team_crawler.away_team)
     if record_away and seeding_away:
         print(f"Away Record: {record_away}\nAway Seeding: {seeding_away}")
     else:
         print("Failed to fetch away record and seeding.")
 
-    last_5_games_results_away = get_last_5_games_results(team_crawler.opponent)
+    last_5_games_results_away = get_last_5_games_results(team_crawler.away_team)
     if last_5_games_results_away:
         print(f"Away Last 5 Games: {last_5_games_results_away}")
     else:
